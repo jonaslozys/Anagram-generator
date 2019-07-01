85262d38bd6f582560792e5b772e588bf5f67375
Jonas Lozys <jonas@LT-LIT-SC-0166.internal.visma.com>
2019-07-01 14:17:57 +0300

    Create implementation as class library project, add file reader class.

---
+++ Anagram Generator/Program.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Implementation;
+
+namespace Anagram_Generator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileReader.ReadFile();
+            
+        }
+    }
+}
---
+++ Implementation/FileReader.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Implementation
+{
+    public static class FileReader
+    {
+        public static void ReadFile()
+        {
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\jonas\Desktop\tasks\Anagram generator\zodynas.txt");
+
+            foreach (string line in lines)
+            {
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
