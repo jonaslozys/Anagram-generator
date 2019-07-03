4d6e60c47274d60f818f47e7a3dfd6aba5f336ce
Jonas Lozys <jonas@LT-LIT-SC-0166.internal.visma.com>
2019-07-03 18:06:41 +0300

    Add dummy Words Repository.

---
+++ Tests/dummy classes/DummyWordsRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Contracts;
+
+namespace Tests.dummy_classes
+{
+    public class DummyWordsRepository
+    {
+        public HashSet<Word> GetWords()
+        {
+            string[] words = new string[] { "pakeleivingas", "pakeleivingos", "pakeleivingos", "žvilgsnis", "seminaras", "Semionovičiumi" };
+
+            HashSet<Word> wordObjects= new HashSet<Word>();
+
+            foreach(string word in words)
+            {
+                wordObjects.Add(new Word(word));
+            }
+
+            return wordObjects;
+        }
+    }
+}
