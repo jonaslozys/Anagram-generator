5033db57c1f9262f203efe0e5002f89118b45836
Jonas Lozys <jonas@LT-LIT-SC-0166.internal.visma.com>
2019-07-03 15:51:32 +0300

    Add unit test for Word class.

--- Tests/WordTests.cs
+++ Tests/WordTests.cs
@@ -6,7 +6,7 @@ using Contracts;
 
 namespace Tests
 {
-    class WordTest1
+    class WordTests
     {
         private string word;
         private Word wordObject;
@@ -16,33 +16,21 @@ namespace Tests
         public void SetupTest1()
         {
             word = "testas";
-            letterRegistry = new Dictionary<char, int> { { 't', 2 }, { 'e', 1 }, { 's', 2 }, { 'a', 1 } };
         }
 
         [Test]
         public void Should_Create_Dictionary_With_Three_Letters()
         {
+            letterRegistry = new Dictionary<char, int> { { 't', 2 }, { 'e', 1 }, { 's', 2 }, { 'a', 1 } };
             wordObject = new Word(word);
             Assert.AreEqual(wordObject.letterRegistry, this.letterRegistry);
         }
-    }
 
-    class WordTest2
-    {
-        private string word;
-        private Word wordObject;
-        Dictionary<char, int> letterRegistry;
-
-        [SetUp]
-        public void SetupTest2()
-        {
-            word = "testas";
-            letterRegistry = new Dictionary<char, int> { { 't', 2 }, { 'r', 1 }, { 's', 2 }, { 'a', 1 } };
-        }
 
         [Test]
         public void Should_Not_Create_Correct_Dictionary()
         {
+            letterRegistry = new Dictionary<char, int> { { 't', 2 }, { 'r', 1 }, { 's', 2 }, { 'a', 1 } };
             wordObject = new Word(word);
             Assert.AreNotEqual(wordObject.letterRegistry, this.letterRegistry);
         }
