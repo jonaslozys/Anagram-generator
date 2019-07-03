0e825da55db40a6806e35ec209da15cf3b7334dd
Jonas Lozys <jonas@LT-LIT-SC-0166.internal.visma.com>
2019-07-03 17:31:06 +0300

    Refactor Word unit tests to cover more cases.

--- Tests/WordTests.cs
+++ Tests/WordTests.cs
@@ -8,38 +8,77 @@ namespace Tests
 {
     class WordTests
     {
-        private string word;
         private Word wordObject;
         Dictionary<char, int> letterRegistry;
 
         [SetUp]
         public void Setup()
         {
-            word = "testas";
         }
 
         [Test]
-        public void Should_Create_Dictionary_With_Three_Letters()
+        [TestCase("medis")]
+        [TestCase("qwerty")]
+        [TestCase("12345")]
+        [TestCase("Kot Letas")]
+        public void Should_Create_Dictionary_With_Three_Letters(string testWord)
         {
-            letterRegistry = new Dictionary<char, int> { { 't', 2 }, { 'e', 1 }, { 's', 2 }, { 'a', 1 } };
-            wordObject = new Word(word);
-            Assert.AreEqual(wordObject.letterRegistry, this.letterRegistry);
+            letterRegistry = new Dictionary<char, int>();
+            foreach (char letter in testWord)
+            {
+                if (letterRegistry.ContainsKey(letter))
+                {
+                    letterRegistry[letter] += 1;
+                }
+                else
+                {
+                    letterRegistry[letter] = 1;
+                }
+            }
+            wordObject = new Word(testWord);
+            Assert.AreEqual(wordObject.letterRegistry, letterRegistry);
         }
 
 
         [Test]
-        public void Should_Not_Create_Correct_Dictionary()
+        [TestCase("medis")]
+        [TestCase("qwerty")] 
+        [TestCase("12345")]
+        [TestCase("Kot Letas")]
+        public void Should_Not_Create_Correct_Dictionary(string testWord)
         {
             letterRegistry = new Dictionary<char, int> { { 't', 2 }, { 'r', 1 }, { 's', 2 }, { 'a', 1 } };
-            wordObject = new Word(word);
-            Assert.AreNotEqual(wordObject.letterRegistry, this.letterRegistry);
+            wordObject = new Word(testWord);
+            Assert.AreNotEqual(wordObject.letterRegistry, letterRegistry);
         }
 
         [Test]
-        public void Should_Correctly_Assign_Word_Property()
+        [TestCase("medis")]
+        [TestCase("qwerty")]
+        [TestCase("12345")]
+        [TestCase("Kot Letas")]
+        public void Should_Correctly_Assign_Word_Property(string testWord)
         {
-            wordObject = new Word(word);
-            Assert.AreEqual(wordObject.word, word);
+            wordObject = new Word(testWord);
+            Assert.AreEqual(wordObject.word, testWord);
+        }
+
+        static Dictionary<char, int> DummyDictionary(string word)
+        {
+            Dictionary<char, int> dummyDictionary = new Dictionary<char, int>();
+            foreach (char letter in word)
+            {
+                if (dummyDictionary.ContainsKey(letter))
+                {
+                    dummyDictionary[letter] += 1;
+                }
+                else
+                {
+                    dummyDictionary[letter] = 1;
+                }
+            }
+
+            return dummyDictionary;
         }
     }
 }
