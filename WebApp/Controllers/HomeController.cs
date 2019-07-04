c023796f5b63f33f5a5a7d8482548968925d3091
Jonas Lozys <jonas@LT-LIT-SC-0166.internal.visma.com>
2019-07-04 11:49:56 +0300

    Display list of angrams on home page.

--- WebApp/Controllers/HomeController.cs
+++ WebApp/Controllers/HomeController.cs
@@ -5,14 +5,41 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using WebApp.Models;
+using AnagramLogic;
+using Contracts;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace WebApp.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IConfiguration _config;
+        private IWordsRepository _wordsRepository;
+        private IAnagramSolver _anagramSolver;
+        private AnagramConfiguration _anagramConfiguration;
+
+        private List<string> _anagrams;
+
+        public HomeController(IConfiguration config)
+        {
+            _config = config;
+            _wordsRepository = new WordsRepository();
+            _anagramConfiguration = new AnagramConfiguration(
+                Int32.Parse(config.GetSection("AnagramSettings:minWordLength").Value),
+                Int32.Parse(config.GetSection("AnagramSettings:maxResultsLength").Value)
+            );
+
+            _anagramSolver = new AnagramSolver(_wordsRepository, _anagramConfiguration);
+        }
         public ActionResult Index(string word)
         {
-            ViewBag.Word = word;
+            if (word != null)
+            {
+                _anagrams = _anagramSolver.GetAnagrams(word);
+                ViewBag.Word = word;
+                ViewBag.Words = _anagrams;
+            }
 
             return View();
         }
--- WebApp/Startup.cs
+++ WebApp/Startup.cs
@@ -30,7 +30,7 @@ namespace WebApp
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
-
+            services.AddSingleton(Configuration);
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
