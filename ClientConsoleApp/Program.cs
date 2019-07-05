30377e38fb8854b64ca42015a74a7c319c5ff08b
Jonas Lozys <jonas@LT-LIT-SC-0166.internal.visma.com>
2019-07-05 15:47:18 +0300

    Create Console App to send GET request to Anagrams API.

--- ClientConsoleApp/Program.cs
+++ ClientConsoleApp/Program.cs
@@ -1,12 +1,45 @@
 ﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace ClientConsoleApp
 {
     class Program
     {
+        private static readonly HttpClient client = new HttpClient();
+
+        public async Task SendRequest(string word)
+        {
+            try
+            {
+                Uri uri = new Uri($"https://localhost:5001/api/anagrams/{word}");
+                Console.WriteLine($"Sending request to {uri}");
+                HttpResponseMessage response = await client.GetAsync(uri);
+                string responseBody = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(response.ToString());
+                Console.WriteLine(responseBody);
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine(e);
+            }
+
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Program program = new Program();
+
+            string userInputString = "";
+
+            foreach (string word in args)
+            {
+                userInputString += word;
+            }
+
+            program.SendRequest(userInputString).Wait();
+
         }
     }
 }
