ce7cdda3dfc3d8b3f00e6dd52803d74752771dd9
Jonas Lozys <jonas@LT-LIT-SC-0166.internal.visma.com>
2019-07-09 13:46:02 +0300

    Add table for cached words.

--- SQL Query/SQLQuery.sql
+++ SQL Query/SQLQuery.sql
@@ -1,12 +1,22 @@
-CREATE TABLE Words 
+IF NOT EXISTS (SELECT * FROM sysobjects where name = 'Words')
+	CREATE TABLE Words 
 	(
 		Id INT IDENTITY(1, 1) NOT NULL,
 		Word NVARCHAR(255) NOT NULL
 	);
 
-CREATE TABLE UserLog
+IF NOT EXISTS (SELECT * FROM sysobjects where name = 'UserLog')
+	CREATE TABLE UserLog
 	(
 		UserIP NVARCHAR(255) NOT NULL,
 		WordSearched NVARCHAR(255) NOT NULL,
 		SearchDate DATETIME NOT NULL DEFAULT (GETDATE())
 	);
+
+IF NOT EXISTS (SELECT * FROM sysobjects where name = 'CachedWords')
+	CREATE TABLE CachedWords
+		(
+			Word VARCHAR(255) NOT NULL,
+			Id INT NOT NULL
+		);
+
