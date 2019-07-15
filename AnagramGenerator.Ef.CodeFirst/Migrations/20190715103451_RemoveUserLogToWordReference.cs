9bcb59b2386e4bb94a512ab2f52f6e9d720138b5
Jonas Lozys <jonas@LT-LIT-SC-0166.internal.visma.com>
2019-07-15 13:37:43 +0300

    Remove reference to word from userlog.

---
+++ AnagramGenerator.Ef.CodeFirst/Migrations/20190715103451_RemoveUserLogToWordReference.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AnagramGenerator.Ef.CodeFirst.Migrations
+{
+    public partial class RemoveUserLogToWordReference : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserLogs_Words_WordSearchedId",
+                table: "UserLogs");
+
+            migrationBuilder.DropIndex(
+                name: "IX_UserLogs_WordSearchedId",
+                table: "UserLogs");
+
+            migrationBuilder.DropColumn(
+                name: "WordSearchedId",
+                table: "UserLogs");
+
+            migrationBuilder.AddColumn<string>(
+                name: "WordSearched",
+                table: "UserLogs",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "WordSearched",
+                table: "UserLogs");
+
+            migrationBuilder.AddColumn<int>(
+                name: "WordSearchedId",
+                table: "UserLogs",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserLogs_WordSearchedId",
+                table: "UserLogs",
+                column: "WordSearchedId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserLogs_Words_WordSearchedId",
+                table: "UserLogs",
+                column: "WordSearchedId",
+                principalTable: "Words",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
