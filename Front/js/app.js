be03421f57a5fc97bc9daba0d84c5f4317226bc2
Jonas Lozys <jonas@LT-LIT-SC-0166.internal.visma.com>
2019-07-25 16:04:20 +0300

    Refactor app.js

--- Front/js/app.js
+++ Front/js/app.js
@@ -1,9 +1,6 @@
 import Renderer from "./renderer.js";
 import HomeController from './controllers/homeController.js';
 import DictionaryController from './controllers/dictionaryController.js';
-import homeView from './views/homeView.js';
-import dictionaryView from './views/dictionaryView.js';
-import getAnagrams from './services/getAnagrams.js';
 
 class App {
     constructor() {
@@ -11,15 +8,13 @@ class App {
     }
 
     init() {
-        this.renderer.render();
-        this.renderer.changePage(homeView());
         this.homeController = new HomeController();
         this.dictionaryController = new DictionaryController();
         this.addEventListenersForNavigationalButtons();
     }
 
     addEventListenersForNavigationalButtons() {
-        document.getElementById("homeLink").addEventListener("click", () => this.renderer.changePage(homeView()));
+        document.getElementById("homeLink").addEventListener("click", () => this.homeController.displayPage());
         document.getElementById("dictionaryLink").addEventListener("click", () => this.dictionaryController.changePage());
         document.getElementById("historyLink").addEventListener("click", () => this.renderer.changePage("History page"));
     }
--- Front/js/controllers/homeController.js
+++ Front/js/controllers/homeController.js
@@ -4,9 +4,8 @@ import homeView from '../views/homeView.js';
 
 class HomeController{
     constructor() {
-        this.submitButton = document.getElementById("buttonGetAnagrams")
-                                    .addEventListener("click", (e) => this.handleSubmit(e));
         this.renderer = new Renderer();
+        this.displayPage();
     }
 
     handleSubmit(e) {
@@ -17,6 +16,15 @@ class HomeController{
         getAnagrams(word)
             .then(anagrams => this.renderer.changePage(homeView(anagrams.data)))
             .catch(err => console.log(err));
+    }
+
+    setupEventListeners() {
+        this.submitButton = document.getElementById("buttonGetAnagrams")
+            .addEventListener("click", (e) => this.handleSubmit(e));
+    }
+
+    displayPage() {
+        this.renderer.changePage(homeView());
     }
     
 }
